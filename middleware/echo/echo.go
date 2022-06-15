08c7afdf931d7bd1440b07f143baae6b5fbe22bb
Dmitry Harupa <d.harupa@pin-up.team>
2022-06-15 12:49:54 +0200

    mw/echo: update for new feature in mw http

--- middleware/echo/echo.go
+++ middleware/echo/echo.go
@@ -1,21 +1,20 @@
 package echo
 
 import (
-	"context"
 	"net/http"
 
 	mw "github.com/d7561985/tel/v2/middleware/http"
 	"github.com/labstack/echo/v4"
 )
 
-type Receiver struct{}
-
 // GetPath extracts path from chi route for http MW for correct metric exposure
-func (Receiver) GetPath(r *http.Request) string {
+func GetPath(r *http.Request) string {
 	return echo.GetPath(r)
 }
 
 // HTTPServerMiddlewareAll all in one mw packet
-func HTTPServerMiddlewareAll(ctx context.Context) echo.MiddlewareFunc {
-	return echo.WrapMiddleware(mw.ServerMiddlewareAll())
+func HTTPServerMiddlewareAll(opts ...mw.Option) echo.MiddlewareFunc {
+	return echo.WrapMiddleware(mw.ServerMiddlewareAll(
+		append(opts, mw.WithPathExtractor(GetPath))...,
+	))
 }
